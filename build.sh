1cd7f20108ed3acd8b813edbfedbc7f47be44eb2
Joern Ott <joern.ott@ott-consult.de>
2018-03-07 14:27:57 +0000

    Add forgotten build.sh

--- build.sh
+++ build.sh
@@ -5,6 +5,24 @@ curl -sSo src/tmp/install/functions.sh https://raw.githubusercontent.com/joernot
 source src/tmp/install/functions.sh
 
 patch_dockerfile
+git clone https://github.com/joernott/semaphore.git
+cd semaphore
+# Workaround while my changes are not back in master
+git checkout feature/both_vaults
+SEMAPHORE_COMMIT=$(git rev-parse HEAD)
+FILES=$(find . -iname "*.go")
+for FILE in ${FILES}; do
+    sed -i ${FILE} -e 's|github.com/ansible-semaphore/semaphore|github.com/joernott/semaphore|g'
+done
+# End workaround
+./make.sh
+cd ..
+if [ ! -d "src/${APP_HOME}" ]; then
+    mkdir -p "src/${APP_HOME}"
+fi
+cp semaphore/cli/semaphore_linux_amd64 "src/${APP_HOME}/semaphore"
+chmod a+x src/${APP_HOME}/semaphore
+sed -i Dockerfile -e "s|SEMAPHORE_COMMIT=.*|SEMAPHORE_COMMIT=${SEMAPHORE_COMMIT} \\|"
 
-docker build -t registry.ott-consult.de/oc/rsyslog:latest .
-docker push registry.ott-consult.de/oc/rsyslog:latest
+docker build -t registry.ott-consult.de/oc/semaphore:latest .
+docker push registry.ott-consult.de/oc/semaphore:latest
